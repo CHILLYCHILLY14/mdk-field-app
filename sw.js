cb43fdf7d7a538adaabb557a43fb1d8c10253e88
CHILLYCHILLY14 <g95tk4g74h@privaterelay.appleid.com>
2026-08-26 23:59:06 -0400

    Lift mobile navigation above Safari toolbar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mdk-field-v6";
+const CACHE = "mdk-field-v7";
 const APP_SHELL = [
   "./", "./index.html", "./styles.css", "./app.js", "./data.js", "./pdf.js",
   "./manifest.webmanifest", "./assets/mdk-logo.jpg", "./assets/icons/icon-192.png",
